6dfc35452924d0a6b39f5b58682c83bf2eeaa25e
Stephen McCurry <stemccurry@gmail.com>
2013-05-22 12:18:16 -0700

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jupiter::Application.config.secret_token = '871423ae0b8927518aa6d5d8fbb076f3f3baed513f0f0ee34065f10284256986549e6ebd64fa6a322fcdb686c85178e9a55beed04e1f0130727c142678ab1b74'
