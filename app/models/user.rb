2f1013f0a1cd1572637574d3e7e66ae4b5e4413a
Stephen McCurry <stemccurry@gmail.com>
2013-05-30 20:46:41 -0700

    inv.to8

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,10 @@ class User < ActiveRecord::Base
   # This is a class method, callable from SessionsController # hence the "User."
   def User.create_with_omniauth(auth)
     user = User.new()
-    user.provider = auth["provider"] user.uid = auth["uid"]
-    user.name = auth["info"]["name"] user.save
+    user.provider = auth["provider"] 
+    user.uid = auth["uid"]
+    user.name = auth["info"]["name"] 
+    user.save
     return user
   end 
 end
