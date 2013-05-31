c205821fca515bcd28563343cf553cf531920436
Stephen McCurry <stemccurry@gmail.com>
2013-05-30 20:55:15 -0700

    inv.to9

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,3 @@
 class Identity < OmniAuth::Identity::Models::ActiveRecord
-attr_accessible :email, :name, :password_digest :password, :password confirmation
+attr_accessible :email, :name, :password, :password_confirmation
 end
