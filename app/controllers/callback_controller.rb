bc6109b99bb2e6d92559d48ff5a4e521ff452e02
Stephen McCurry <stemccurry@gmail.com>
2013-07-13 23:38:17 -0700

    response3

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -4,15 +4,15 @@ class CallbackController < ApplicationController
   def create
     puts "CALLBACK INITIATED"
     
-    if params[:SmsStatus].blank?
-      response = SmsResponse.new(:SMSId => params[:SMSId], :AccountSid => params[:AccountSid], :To => params[:To], :From => params[:From], :Body => params[:Body], :FromCity => params[:FromCity], :FromState => params[:FromState], :FromZIP => params[:FromZIP], :FromCountry => params[:FromCountry])
+    if params[:SmsStatus].blank? || params[:SmsStatus]='received'
+      @sms = SmsResponse.new(:SMSId => params[:SmsSid], :AccountSid => params[:AccountSid], :To => params[:To], :From => params[:From], :Body => params[:Body], :FromCity => params[:FromCity], :FromState => params[:FromState], :FromZIP => params[:FromZIP], :FromCountry => params[:FromCountry])
     else 
-      puts "CALLBACK STATUS #{params[:SmsStatus]} SmsId #{params[:SMSId]}"
+      puts "CALLBACK STATUS #{params[:SmsStatus]} SmsId #{params[:SmsSid]}"
     end
    
-    if !response.save then
+    if @sms.nil? || !@sms.save then
       puts "CALLBACK ERROR!"
-      puts response.errors.full_messages
+      puts @sms.errors.full_messages
     end
     
     render nothing: true
