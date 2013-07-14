5f8fb1be96ae964fc88ce5d73ae3e7fe54add2ff
Stephen McCurry <stemccurry@gmail.com>
2013-07-14 00:29:41 -0700

    response6

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -4,7 +4,7 @@ class CallbackController < ApplicationController
   def create
     puts "CALLBACK INITIATED"
     
-    if params[:SmsStatus].blank? || params[:SmsStatus]='received'
+    if params[:SmsStatus].blank? || params[:SmsStatus]=='received'
       @sms = SmsResponse.new(:SMSId => params[:SmsSid], :AccountSid => params[:AccountSid], :To => params[:To], :From => params[:From], :Body => params[:Body], :FromCity => params[:FromCity], :FromState => params[:FromState], :FromZIP => params[:FromZIP], :FromCountry => params[:FromCountry])
     else 
       puts "CALLBACK STATUS #{params[:SmsStatus]} SmsId #{params[:SmsSid]}"
