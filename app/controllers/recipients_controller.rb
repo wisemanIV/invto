a910b4f57b436be1ad60ed767389e4db187b0491
Stephen McCurry <stemccurry@gmail.com>
2013-07-17 00:35:19 -0700

    admin3

--- app/controllers/recipients_controller.rb
+++ app/controllers/recipients_controller.rb
@@ -1,6 +1,7 @@
 class RecipientsController < ApplicationController
   before_filter :authenticate_user!
-  
+  load_and_authorize_resource
+   
   # GET /recipients
   # GET /recipients.json
   def index
