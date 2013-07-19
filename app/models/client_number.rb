7ed8dd7c12d994d2504e9487a27a547312f20aed
Stephen McCurry <stemccurry@gmail.com>
2013-07-18 17:49:45 -0700

    client numbers3

--- app/models/client_number.rb
+++ app/models/client_number.rb
@@ -1,5 +1,6 @@
 class ClientNumber < ActiveRecord::Base
   attr_accessible :client_id, :phone
+  validates :phone, :uniqueness => true, :presence => true
   belongs_to :client, :dependent => :destroy
   
 end
