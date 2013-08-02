a3700e26fd343510cd8ca70d65caae4224e9d19d
Stephen McCurry <stemccurry@gmail.com>
2013-08-02 13:50:30 -0700

    archives4

--- test/factories.rb
+++ test/factories.rb
@@ -2,4 +2,15 @@ FactoryGirl.define do
   factory :message do
     body "a random message"
   end
+  factory :sms_response do
+    Body "a random message"
+    AccountSid "233443434"
+    From '+14154200068'
+    SMSId "3434343535"
+    To '+14154200068'
+      
+  end
+  factory :client do
+    domain "www.inv.to"
+  end
 end
