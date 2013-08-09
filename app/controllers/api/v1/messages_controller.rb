9ee681e6d03117e7fd75f6d65868d44dfa6eaed9
Stephen McCurry <stemccurry@gmail.com>
2013-08-09 00:13:47 -0700

    upgrade7

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -24,21 +24,16 @@ module Api
 
       def create 
   
-        client = Client.find(current_user.client_id)
-
-        if client.nil? 
-          respond_with "Error: Cannot create SMS until an application is provisioned on your account.'e".to_json
-        else
           # send an sms
           saved = true  
     
           if Message.is_valid_phone(params[:message][:to])
-            status = $MESSAGE_STATUS[0] #submitted
+            status = $MESSAGE_STATUS[1] #submitted
           else 
             status = $MESSAGE_STATUS[7] #invalid phone
           end
   
-          @message = Message.new(:attachment => params[:message][:attachment], :campaign => params[:message][:campaign], :version => params[:message][:version], :to => params[:message][:to], :body => params[:message][:body], :status => status, :user_id => current_user.id, :client_id => client.id )
+          @message = Message.new(:attachment => params[:message][:attachment], :campaign => params[:message][:campaign], :version => params[:message][:version], :to => params[:message][:to], :body => params[:message][:body], :status => status, :user_id => current_user.id, :client_id => current_user.client_id )
   
           if !@message.save
             respond_with "error saving message".to_json
@@ -46,7 +41,7 @@ module Api
             @message.delay.send_sms!
             respond_with @message
           end
-        end
+ 
       end
 
     end
